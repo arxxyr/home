f143ad8eb05f98ac4298d883e34389f918ca8ddb
fab <fin.albis@gmail.com>
2018-03-14 22:37:24 +0800

    Update download-complete.sh

--- scripts/download-complete.sh
+++ scripts/download-complete.sh
@@ -1,22 +1,29 @@
-#!/bin/bash
-path=$3
-downloadpath='/media/extraDisk/aria2c/downloading'
-donepath='/media/extraDisk/aria2c/done'
-
-if [$2 -eq 0]
-        then
-                exit 0
+#!/bin/sh
+ 
+# $1 is gid.
+# $2 is the number of files.
+# $3 is the path of the first file.
+ 
+DOWNLOAD=/media/extraDisk/aria2c/downloading # no trailing slash!
+COMPLETE=/media/extraDisk/aria2c/done # no trailing slash!
+LOG=/media/extraDisk/aria2c/logs/mvcompleted.log
+SRC=$3
+ 
+if [ "$2" == "0" ]; then
+  echo `date` "INFO  no file to move for" "$1". >> "$LOG"
+  exit 0
 fi
+ 
 while true; do
-filepath=$path
-path=${path%/*}; 
-if ["$path" = "$downloadpath"] && [ $2 -eq 1 ]
-    then
-    mv "$filepath" "$donepath"      
-    exit 0
-elif ["$path" = "$downloadpath"]
-    then
-    mv "$filepath"/ "$donepath"/"${filepath##*/}"/
-    exit 0
-fi
+  DIR=`dirname "$SRC"`
+  if [ "$DIR" == "$DOWNLOAD" ]; then
+    echo `date` "INFO " "$3" moved as "$SRC". >> "$LOG"
+    mv --backup=t "$SRC" "$COMPLETE" >> "$LOG" 2>&1
+    exit $?
+  elif [ "$DIR" == "/" -o "$DIR" == "." ]; then
+    echo `date` ERROR "$3" not under "$DOWNLOAD". >> "$LOG"
+    exit 1
+  else
+    SRC=$DIR
+  fi
 done
